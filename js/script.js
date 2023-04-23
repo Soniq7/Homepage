48868d3224a48e538a3de7dfee1f1a4f52e85f4c
Sonia Skowron-Zarebska <ssoniq7@gmail.com>
2023-04-23 22:41:49 +0100

    js functions added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-console.log("Hi all! Hope exploring this page will be as sweet as honey! 😎🍯");
+{
+    const welcome = () => {
+        console.log("Hi all! Hope exploring this page will be as sweet as honey! 😎🍯");
+    };
 
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
+    const toggleBackground = () => {
+        const button = document.querySelector(".js-button");
+        const body = document.querySelector(".js-body");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("darkMode");
-    button.classList.toggle("darkMode");
-    button.innerText = (button.innerText === "Dark mode") ? "Light mode" : "Dark mode";
-    });
+        body.classList.toggle("darkMode");
+        button.classList.toggle("darkMode");
+        button.innerText = (button.innerText === "Dark mode") ? "Light mode" : "Dark mode";
+        
+        button.addEventListener("click", toggleBackground);
+    };
 
+    welcome();
+    toggleBackground();
+
+}
 
 
 
